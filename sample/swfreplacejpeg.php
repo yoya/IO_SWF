e831201b698622971ffa8f84f7e445e937f0cb8e
よや <yoya@awm.jp>
2010-08-16 08:36:07 +0000

    引数で image_id (= character id) を指定するようにした

--- sample/swfreplacejpeg.php
+++ sample/swfreplacejpeg.php
@@ -1,28 +1,29 @@
 <?php
 
-  //require 'IO/SWF.php';
 require 'IO/SWF/Editor.php';
 
-if ($argc != 3) {
-    echo "Usage: php swfreplacejpeg.php <swf_file> <jpeg_file>\n";
-    echo "ex) php swfreplacejpeg.php test.swf test.jpg\n";
+if ($argc != 4) {
+    echo "Usage: php swfreplacejpeg.php <swf_file> <image_id> <jpeg_file>\n";
+    echo "ex) php swfreplacejpeg.php test.swf 1 test.jpg\n";
     exit(1);
 }
 
 assert(is_readable($argv[1]));
-assert(is_readable($argv[2]));
-
+assert(is_numeric($argv[2]));
+assert(is_readable($argv[3]));
 
 $swfdata = file_get_contents($argv[1]);
+$image_id = (int) $argv[2];
 
 $swf = new IO_SWF_Editor();
 $swf->parse($swfdata);
 $swf->setCharacterId($swfdata);
 
 $erroreous_header = pack('CCCC', 0xFF, 0xD9, 0xFF, 0xD8);
-$jpegdata = file_get_contents($argv[2]);
+$jpegdata = file_get_contents($argv[3]);
 
-$swf->replaceTagContentByCharacterId(21, 1, $erroreous_header.$jpegdata);
+// 21: DefineBitsJPEG2
+$swf->replaceTagContentByCharacterId(21, $image_id, $erroreous_header.$jpegdata);
 
 echo $swf->build();
 
