d88b532d5fc8d4a13ff2cc20a3d9a4c0c75614e8
よや <yoya@awm.jp>
2010-08-23 16:13:47 +0000

    - SWF 特有の JPEG 操作を行うコマンド

---
+++ sample/jpeg_dump.php
@@ -0,0 +1,34 @@
+<?php
+
+require_once 'IO/SWF/JPEG.php';
+
+function usage() {
+    echo "Usage: php jpeg_dump.php <dump|jpegtables|imagedata>".PHP_EOL;
+}
+
+if ($argc != 3) {
+    usage();
+    exit(1);
+}
+
+$jpegdata = file_get_contents($argv[2]);
+
+$jpeg = new IO_SWF_JPEG();
+$jpeg->input($jpegdata);
+
+switch($argv[1]) {
+  case 'dump':
+    $jpeg->dumpChunk();
+    break;
+  case 'jpegtables':
+    echo $jpeg->getEncodingTables();
+    break;
+  case 'imagedata':
+    echo $jpeg->getImageData();
+    break;
+  default:
+    usage();
+    exit(1);
+}
+
+exit(0);
