6540a3e0d60ef1c734add23e4d1dbc7f200181f7
よや <yoya@awm.jp>
2011-01-26 15:24:02 +0000

    SWF の基本データ型の perse/build メソッド

---
+++ IO/SWF/Type.php
@@ -0,0 +1,67 @@
+<?php
+
+/*
+ * 2011/1/25- (c) yoya@awm.jp
+ */
+
+require_once 'IO/Bit.php';
+
+class IO_SWF_Type {
+    static function parseRECT($reader) {
+        $frameSize = array();
+        $nBits = $reader->getUIBits(5);
+        $frameSize['(NBits)'] = $nBits;
+        $frameSize['Xmin'] = $reader->getSIBits($nBits);
+        $frameSize['Xmax'] = $reader->getSIBits($nBits);
+        $frameSize['Ymin'] = $reader->getSIBits($nBits);
+        $frameSize['Ymax'] = $reader->getSIBits($nBits) ;
+    	return $frameSize; 
+    }
+    static function buildRECT($data) {
+    	   return '';
+    }
+    static function parseRGB($reader) {
+    	$rgb = array();
+    	$rgb['Red'] = $reader->getUI8();
+    	$rgb['Green'] = $reader->getUI8();
+    	$rgb['Blue'] = $reader->getUI8();
+	return $rgb;
+    }
+    static function buildRGB($d) {
+    	   return '';
+    }
+    static function parseRGBA($reader) {
+    	$rgba = array();
+    	$rgba['Red'] = $reader->getUI8();
+    	$rgba['Green'] = $reader->getUI8();
+    	$rgba['Blue'] = $reader->getUI8();
+    	$rgba['Alpha'] = $reader->getUI8();
+	return $rgba;
+    }
+    static function buildRGBA($d) {
+    	   return '';
+    }
+    static function parseMATRIX($reader) {
+    	$matrix = array();
+        $hasScale = $reader->getUIBit();
+	if ($hasScale) {
+	    $nScaleBits = $reader->getUIBits(5);
+	    $matrix['(NScaleBits)'] = $nScaleBits;
+	    $matrix['ScaleX'] = $reader->getSIBits($nScaleBits);
+	    $matrix['ScaleY'] = $reader->getSIBits($nScaleBits);
+	}
+        $hasRotate = $reader->getUIBit();
+	if ($hasRotate) {
+	    $nRotateBits = $reader->getUIBits(5);
+	    $matrix['(NRotateBits)'] = $nRotateBits;
+	    $matrix['RotateSkew0'] = $reader->getSIBits($nRotateBits);
+	    $matrix['RotateSkew1'] = $reader->getSIBits($nRotateBits);
+	}
+        $nTranslateBits = $reader->getUIBits(5);
+	$matrix['TranslateX'] = $reader->getSIBits($nTranslateBits);
+	$matrix['TranslateY'] = $reader->getSIBits($nTranslateBits);
+    }
+    static function buildMATRIX($d) {
+    	   return '';
+    }
+}
