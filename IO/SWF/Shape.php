975d73124fa71eec02797cc707f0f6e8d16eca61
よや <yoya@awm.jp>
2011-01-26 16:54:05 +0000

    - Gradient Style の parse を実装した - ShapeRecords の STYLECHANGERECORD と ENDSHAPERECOERD の parse を実装した - ShapeRecords の EDGE は途中。

--- IO/SWF/Shape.php
+++ IO/SWF/Shape.php
@@ -12,6 +12,65 @@ class IO_SWF_Shape {
 	$reader->input($content);
         $this->_shapeId = $reader->getUI16LE();
     	$this->_shapeBounds = IO_SWF_Type::parseRECT($reader);
+
+	$this->_parseFILLSTYLEARRAY($reader);
+	$this->_parseLINESTYLEARRAY($reader);
+
+	$numFillBits = $reader->getUIBits(4);
+	$numLineBits = $reader->getUIBits(4);
+	$done = false;
+	while ($done) {
+	    $shapeRecord = array();
+	    $typeFlag = $reader->getUIBit();
+	    $shapeRecord['TypeFlag'] = $typeFlag;
+	    if ($typeFlag == 0) {
+	        $endOfShape = $reader->getUIBits(5); // XXX not 4 ?
+		if ($endOfShape == 0) {
+		    $shapeRecord['EndOfShape'] = $endOfShape;
+		    $done = true;
+		} else {
+		    $reader->incrementOffset(0, -5); // XXX not 4 ?
+		    $shapeRecord['StateNewStyles'] = $reader->getUIBit();
+		    $shapeRecord['StateLineStyle'] = $reader->getUIBit();
+		    $shapeRecord['StateFillStyle1'] = $reader->getUIBit();
+		    $shapeRecord['StateFillStyle0'] = $reader->getUIBit();
+		    //
+		    $shapeRecord['StateMoveTo'] = $reader->getUIBit();
+		    if ($shapeRecord['StateMoveTo']) {
+		        $moveBits = $reader->getUIBits(5);
+		    	$shapeRecord['(MoveBits)'] = $moveBits;
+			$shapeRecord['MoveDeltaDeltaX'] = $reader->getUIBits($moveBits);
+			$shapeRecord['MoveDeltaDeltaY'] = $reader->getUIBits($moveBits);
+		    }
+		    if ($shapeRecord['StateFillStyle0']) {
+		        $shapeRecord['FillStyle0'] = $reader->getUIBits($numFillBits);
+		    }
+		    if ($shapeRecord['StateFillStyle1']) {
+		        $shapeRecord['FillStyle1'] = $reader->getUIBits($numFillBits);
+		    }
+		    if ($shapeRecord['StateLineStyle']) {
+		        $shapeRecord['LineStyle'] = $reader->getUIBits($numLineBits);
+		    }
+		    if ($shapeRecord['StateNewStyles']) {
+		    	$this->_parseFILLSTYLEARRAY($reader);
+			$this->_parseLINESTYLEARRAY($reader);
+			$numFillBits = $reader->getUIBits(4);
+			$numLineBits = $reader->getUIBits(4);
+		    }
+		}
+	    } else { // Edge
+	        $shapeRecord['StraightFlag'] = $reader->getUIBit();
+		if ($shapeRecord['StraightFlag']) {
+		       ;
+		} else {
+		       ;
+		}
+	    }
+	    $this->_shapeRecords []= $shapeRecord;
+	}
+
+    }
+    function _parseFILLSTYLEARRAY($reader) {
 	// FillStyle
 	$fillStyleCount = $reader->getUI8();
 	if (($tagCode > 2) && ($fillStyleCount == 0xff)) {
@@ -63,7 +122,8 @@ class IO_SWF_Shape {
 	    }
 	    $this->_fillStyles[] = $fillStyle;
 	}
-	// LineStyle
+    }
+    function _parseLINESTYLEARRAY($reader) {
 	$lineStyleCount = $reader->getUI8();
 	if (($tagCode > 2) && ($lineStyleCount == 0xff)) {
 	   // DefineShape2 以降は 0xffff サイズまで扱える
@@ -79,9 +139,6 @@ class IO_SWF_Shape {
 	    }
 	    $this->_lineStyles[] = $lineStyle;
         }
-	$numfillBits = 0;
-	$numLineBits = 0;
-
     }
     function dump() {
     	echo "ShapeId: {$this->_shapeId}\n";
