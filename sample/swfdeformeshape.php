a419526cc1cfc62aeedb26c5b9daf5472edcabc0
よや <yoya@awm.jp>
2011-01-31 18:14:08 +0000

    Shape をデフォルメするツール。(まだデフォルメせずにバイナリを再構築しているだけ)

---
+++ sample/swfdeformeshape.php
@@ -0,0 +1,26 @@
+<?php
+
+require dirname(__FILE__).'/../IO/SWF/Editor.php';
+// require 'IO/SWF/Editor.php';
+
+if ($argc != 3) {
+    fprintf(STDERR, "Usage: php swfdeformeshape.php <swf_file> <threshold>\n");
+    fprintf(STDERR, "ex) php swfdeformeshape.php test.swf 10 \n");
+    exit(1);
+}
+
+assert(is_readable($argv[1]));
+assert(is_numeric($argv[2]));
+
+$swfdata = file_get_contents($argv[1]);
+$threshold = $argv[2];
+
+$swf = new IO_SWF_Editor();
+
+$swf->parse($swfdata);
+
+$swf->deformeShape($threshold);
+
+echo $swf->build();
+
+exit(0);
