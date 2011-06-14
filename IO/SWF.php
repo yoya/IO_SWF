b43d51393ad9b029cffc2fa337698867b2f28a57
よや <yoya@awm.jp>
2011-06-14 13:11:58 +0000

    SWF_Info を作って dump メソッドを移動

--- IO/SWF.php
+++ IO/SWF.php
@@ -58,32 +58,6 @@ class IO_SWF {
         }
         return true;
     }
-
-    function dump($opts = array()) {
-        if (empty($opts['hexdump']) === false) {
-            $bitio = new IO_Bit();
-            $bitio->input($this->_swfdata);
-        }
-        /* SWF Header */
-        echo 'Signature: '.$this->_headers['Signature'].PHP_EOL;
-        echo 'Version: '.$this->_headers['Version'].PHP_EOL;
-        echo 'FileLength: '.$this->_headers['FileLength'].PHP_EOL;
-        echo 'FrameSize: '. IO_SWF_Type_RECT::string($this->_headers['FrameSize'])."\n";
-        echo 'FrameRate: '.($this->_headers['FrameRate'] / 0x100).PHP_EOL;
-        echo 'FrameCount: '.$this->_headers['FrameCount'].PHP_EOL;
-
-        if (empty($opts['hexdump']) === false) {
-            $bitio->hexdump(0, $this->_header_size);
-            $opts['bitio'] =& $bitio; // for tag
-        }
-
-        /* SWF Tags */
-        
-        echo 'Tags:'.PHP_EOL;
-        foreach ($this->_tags as $tag) {
-    	    $tag->dump($opts);
-        }
-    }
     
     function build() {
         $writer_head = new IO_Bit();
