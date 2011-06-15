7a4d5082c9ddafbb1d11b206d2cf6627f49478d3
よや <yoya@awm.jp>
2011-06-15 07:45:32 +0000

    Bitmap の画像サイズを取得するメソッドを追加

---
+++ IO/SWF/Bitmap.php
@@ -0,0 +1,84 @@
+<?php
+
+/*
+ * Bitmap Utility Routine
+ */
+
+require_once 'IO/Bit.php';
+
+class IO_SWF_Bitmap {
+    static function get_jpegsize($jpegdata) {
+        $chunk_length = 0;
+        $jpegdata_len = strlen($jpegdata);
+        for ($idx = 0 ; (($idx + 8) < $jpegdata_len) ; $idx += $chunk_length) {
+            $marker1 = ord($jpegdata[$idx]);
+            if ($marker1 != 0xFF) {
+                break;
+            }
+            $marker2 = ord($jpegdata[$idx + 1]);
+            switch ($marker2) {
+              case 0xD8: // SOI (Start of Image)
+              case 0xD9: // EOI (End of Image)
+                $chunk_length = 2;
+                break;
+              case 0xDA: // SOS
+                return false; // not found
+              case 0xC0: // SOF0
+              case 0xC1: // SOF1
+              case 0xC2: // SOF2
+              case 0xC3: // SOF3
+              case 0xC5: // SOF5
+              case 0xC6: // SOF6
+              case 0xC7: // SOF7
+              case 0xC9: // SOF9
+              case 0xCA: // SOF10
+              case 0xCB: // SOF11
+              case 0xCD: // SOF13
+              case 0xCE: // SOF14
+              case 0xCF: // SOF15
+                $width  = 0x100 * ord($jpegdata[$idx + 7]) + ord($jpegdata[$idx + 8]);
+                $height = 0x100 * ord($jpegdata[$idx + 5]) + ord($jpegdata[$idx + 6]);
+                return array('width' => $width, 'height' => $height); // success
+              default:
+                $chunk_length = 0x100 * ord($jpegdata[$idx + 2]) + ord($jpegdata[$idx + 3]) + 2;
+                if ($chunk_length == 0) { // fail safe;
+                    break;
+                }
+            }
+        }
+        return false; // NG
+    }
+    
+    static function get_pngsize($pngdata) {
+        $pngdata_len = strlen($pngdata);
+        if ($pngdata_len< 24) {
+            fprintf(stderr, "IO_SWF_Bitmap::get_pngsize: data_len(%lu) < 16\n", $pngdata_len);
+            return 1;
+        }
+        $width = (((ord($pngdata[16])*0x100) + ord($pngdata[17]))*0x100 + ord($pngdata[18]))*0x100 + ord($pngdata[19]);
+        $height =(((ord($pngdata[20])*0x100) + ord($pngdata[21]))*0x100 + ord($pngdata[22]))*0x100 + ord($pngdata[23]);
+        return array('width' => $width, 'height' => $height); // success
+    }
+    
+    static function get_gifsize($gifdata) {
+        $gifdata_len = strlen($gifdata);
+        if ($gifdata_len < 10) {
+            fprintf(stderr, "IO_SWF_Bitmap::get_gifsize: data_len(%lu) < 10\n", $gifdata_len);
+            return false;
+        }
+        $width  = 0x100 * ord($gifdata[7]) + ord($gifdata[6]);
+        $height = 0x100 * ord($gifdata[9]) + ord($gifdata[8]);
+        return array('width' => $width, 'height' => $height); // success
+    }
+    
+    static function get_bitmapsize($bitmapdata) {
+        if (strncmp($bitmapdata, "\xff\xd8\xff", 3) == 0) { // JPEG
+            return self::get_jpegsize($bitmapdata);
+        } elseif (substr($bitmapdata, 1, 3) == 'PNG') { // PNG
+            return self::get_pngsize($bitmapdata);
+        } elseif (strncmp($bitmapdata, 'GIF', 3) == 0) { // GIF
+            return self::get_gifsize($bitmapdata);
+        }
+        return false; // NG
+    }
+  }
---
+++ sample/get_bitmapsize.php
@@ -0,0 +1,16 @@
+<?php
+
+require_once 'IO/SWF/Bitmap.php';
+// require_once './IO/SWF/Bitmap.php';
+
+if ($argc != 2) {
+    echo "Usage: php get_bitmapsize.php <bitmap_file>\n";
+    echo "ex) php get_bitmapsize.php test.jpg\n";
+    exit(1);
+}
+
+$bitmap_data = file_get_contents($argv[1]);
+
+$ret = IO_SWF_Bitmap::get_bitmapsize($bitmap_data);
+
+echo "width:{$ret['width']} height:{$ret['height']}\n";
