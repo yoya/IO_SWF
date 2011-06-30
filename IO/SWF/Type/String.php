243574dfd2ed2676caf46821874012f78a6b399c
よや <yoya@awm.jp>
2011-06-30 12:03:15 +0000

    PHP は string の途中に \0 を置けるのでその対処 - parse で \0 がない場合に parse を終える対処 - build で \0 があった場合に \0 から後ろを捨てる対処

--- IO/SWF/Type/String.php
+++ IO/SWF/Type/String.php
@@ -10,7 +10,7 @@ require_once dirname(__FILE__).'/../Type.php';
 class IO_SWF_Type_String extends IO_SWF_Type {
     static function parse(&$reader, $opts = array()) {
         $str = '';
-        while (true) {
+        while ($reader->hasNextData(1)) {
             $c = $reader->getData(1);
             if ($c === "\0") {
                 break;
@@ -20,6 +20,8 @@ class IO_SWF_Type_String extends IO_SWF_Type {
     	return $str;
     }
     static function build(&$writer, $str, $opts = array()) {
+        $strs = explode("\0", $str);
+        $str = $strs[0];
         $writer->putData($str."\0", strlen($str) + 1);
     }
     static function string($str, $opts = array()) {
