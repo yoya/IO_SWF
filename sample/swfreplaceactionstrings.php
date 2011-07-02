d268a4883a71114cbd273369df2eadf1df54d923
よや <yoya@awm.jp>
2011-07-02 15:53:56 +0000

    replaceActionString から replaceActionStrings に変更したので、ファイル名にも反映

--- sample/swfreplaceactionstrings.php
+++ sample/swfreplaceactionstrings.php
@@ -4,8 +4,8 @@ require 'IO/SWF/Editor.php';
 // require dirname(__FILE__).'/../IO/SWF/Editor.php';
 
 if ($argc != 4) {
-    fprintf(STDERR, "Usage: php swfreplaceactionstring.phpp <swf_file> <from_str> <to_str>\n");
-    fprintf(STDERR, "ex) php swfreplaceactionstring.php test.swf foo baa\n");
+    fprintf(STDERR, "Usage: php swfreplaceactionstrings.php <swf_file> <from_str> <to_str>\n");
+    fprintf(STDERR, "ex) php swfreplaceactionstrings.php test.swf foo baa\n");
     exit(1);
 }
 
@@ -22,7 +22,6 @@ $swf = new IO_SWF_Editor();
 $swf->parse($swfdata);
 
 $swf->replaceActionStrings($from_str, $to_str);
-// $swf->replaceActionString($from_str, $to_str); // obsolute
 
 echo $swf->build();
 
