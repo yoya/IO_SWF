4d5551e414173abaa885b336df80b92e2f6ba189
よや <yoya@awm.jp>
2011-07-10 15:22:15 +0000

    - 表示の %x を %d に変更 - インデント修正

--- IO/SWF/Type/CXFORM.php
+++ IO/SWF/Type/CXFORM.php
@@ -14,41 +14,41 @@ class IO_SWF_Type_CXFORM extends IO_SWF_Type {
     	$hasMultiTerms = $reader->getUIBit();
     	$cxform['HasAddTerms'] = $hasAddTerms;
     	$cxform['HasMultiTerms'] = $hasMultiTerms;
-	$nbits = $reader->getUIBits(4);
-	if ($hasMultiTerms) {
-	    $cxform['RedMultiTerm']   = $reader->getSIBits($nbits);
-	    $cxform['GreenMultiTerm'] = $reader->getSIBits($nbits);
-	    $cxform['BlueMultiTerm']  = $reader->getSIBits($nbits);
-	}
-	if ($hasAddTerms) {
-	    $cxform['RedAddTerm']   = $reader->getSIBits($nbits);
-	    $cxform['GreenAddTerm'] = $reader->getSIBits($nbits);
-	    $cxform['BlueAddTerm']  = $reader->getSIBits($nbits);
-	}
+        $nbits = $reader->getUIBits(4);
+        if ($hasMultiTerms) {
+            $cxform['RedMultiTerm']   = $reader->getSIBits($nbits);
+            $cxform['GreenMultiTerm'] = $reader->getSIBits($nbits);
+            $cxform['BlueMultiTerm']  = $reader->getSIBits($nbits);
+        }
+        if ($hasAddTerms) {
+            $cxform['RedAddTerm']   = $reader->getSIBits($nbits);
+            $cxform['GreenAddTerm'] = $reader->getSIBits($nbits);
+            $cxform['BlueAddTerm']  = $reader->getSIBits($nbits);
+        }
     	return $cxform;
     }
     static function build(&$writer, $cxform, $opts = array()) {
         $nbits = 0;
-	$hasAddTerms = 0;
-	$hasMultiTerms = 0;
-	$multi_term_list = array('RedMultiTerm', 'GreenMultiTerm', 'BlueMultiTerm');
-	foreach ($multi_term_list as $term) {
-	    if (isset($cxform[$term])) {
-	        $hasMultiTerms = 1;
-		$need_bits = $writer->need_bits_signed($cxform[$term]);
-		if ($nbits < $need_bits){
-		    $nbits = $need_bits;
-		}
+        $hasAddTerms = 0;
+        $hasMultiTerms = 0;
+        $multi_term_list = array('RedMultiTerm', 'GreenMultiTerm', 'BlueMultiTerm');
+        foreach ($multi_term_list as $term) {
+            if (isset($cxform[$term])) {
+                $hasMultiTerms = 1;
+                $need_bits = $writer->need_bits_signed($cxform[$term]);
+                if ($nbits < $need_bits){
+                    $nbits = $need_bits;
+                }
 	    }
 	}
 	$add_term_list = array('RedAddTerm', 'GreenAddTerm', 'BlueAddTerm');
 	foreach ($add_term_list as $term) {
 	    if (isset($cxform[$term])) {
 	        $hasAddTerms = 1;
-		$need_bits = $writer->need_bits_signed($cxform[$term]);
-		if ($nbits < $need_bits){
-		    $nbits = $need_bits;
-		}
+            $need_bits = $writer->need_bits_signed($cxform[$term]);
+            if ($nbits < $need_bits){
+                $nbits = $need_bits;
+            }
 	    }
 	}
 	$writer->putUIBit($hasAddTerms);
@@ -66,19 +66,19 @@ class IO_SWF_Type_CXFORM extends IO_SWF_Type {
 	}
     }
     static function string($cxform, $opts = array()) {
-      if (($cxform['HasMultiTerms'] == 0) && ($cxform['HasAddTerms'] == 0)) {
-	  return '(No Data: CXFORM)';
-      }
-      $text = '';
-      if ($cxform['HasMultiTerms']) {
-	  $text .= sprintf("MultiTerms:(%02x,%02x,%02x)", $cxform['RedMultiTerm'], $cxform['GreenMultiTerm'], $cxform['BlueMultiTerm']);
-      }
-      if ($cxform['HasAddTerms']) {
-	  if ($cxform['HasMultiTerms']) {
-	      $text .= ' ';
-	  }
-	  $text .= sprintf("AddTerms:(%02x,%02x,%02x)", $cxform['RedAddTerm'], $cxform['GreenAddTerm'], $cxform['BlueAddTerm']);
-      }
-      return $text;
+        if (($cxform['HasMultiTerms'] == 0) && ($cxform['HasAddTerms'] == 0)) {
+            return '(No Data: CXFORM)';
+        }
+        $text = '';
+        if ($cxform['HasMultiTerms']) {
+            $text .= sprintf("MultiTerms:(%d,%d,%d)", $cxform['RedMultiTerm'], $cxform['GreenMultiTerm'], $cxform['BlueMultiTerm']);
+        }
+        if ($cxform['HasAddTerms']) {
+            if ($cxform['HasMultiTerms']) {
+                $text .= ' ';
+            }
+            $text .= sprintf("AddTerms:(%d,%d,%d)", $cxform['RedAddTerm'], $cxform['GreenAddTerm'], $cxform['BlueAddTerm']);
+        }
+        return $text;
     }
 }
