af81bbf9fb9301b4572e635a27fce3726123c6d8
よや <yoya@awm.jp>
2011-08-19 11:56:12 +0000

    build, string メソッドの実装。(コピペ前のがそのまま残ってた)

--- IO/SWF/Type/BUTTONRECORD.php
+++ IO/SWF/Type/BUTTONRECORD.php
@@ -6,6 +6,7 @@
 
 require_once 'IO/Bit.php';
 require_once dirname(__FILE__).'/../Type.php';
+require_once dirname(__FILE__).'/CXFORM.php';
 require_once dirname(__FILE__).'/CXFORMWITHALPHA.php';
 
 class IO_SWF_Type_BUTTONRECORD extends IO_SWF_Type {
@@ -13,84 +14,84 @@ class IO_SWF_Type_BUTTONRECORD extends IO_SWF_Type {
     	$buttonrecord = array();
 
         $buttonrecord['ButtonReserved'] = $reader->getUIBits(2); // must be 0
-	$buttonHasBlendMode = $reader->getUIBit();
-	$buttonHasFilterList = $reader->getUIBit(); 
+        $buttonHasBlendMode = $reader->getUIBit();
+        $buttonHasFilterList = $reader->getUIBit(); 
         $buttonrecord['ButtonHasBlandMode'] = $buttonHasBlendMode;
         $buttonrecord['ButtonHasFilterList'] = $buttonHasFilterList;
         $buttonrecord['ButtonStateHitTest'] = $reader->getUIBit(); 
         $buttonrecord['ButtonStateDown'] = $reader->getUIBit(); 
         $buttonrecord['ButtonStateOver'] = $reader->getUIBit(); 
         $buttonrecord['ButtonStateUp'] = $reader->getUIBit(); 
-	//
-	$buttonrecord['CharacterID'] = $reader->getUI16LE();
-	$buttonrecord['PlaceDepth'] = $reader->getUI16LE();
-	$buttonrecord['PlaceMatrix'] = IO_SWF_Type_MATRIX::parse($reader);
-	if ($opts['tagCode'] == 34) { // DefineButton2
-	    $buttonrecord['ColorTransform'] = IO_SWF_Type_CXFORMWITHALPHA::parse($reader);
-	} else {
-	    $buttonrecord['ColorTransform'] = IO_SWF_Type_CXFORM::parse($reader);
-	}
-	if (($opts['tagCode'] == 34) &&  // DefineButton2
-	    ($buttonHasFilterList == 1)) {
-	    $buttonrecord['FilterList'] = IO_SWF_Type_FILTERLIST::parse($reader);
-	}
-	if (($opts['tagCode'] == 34) &&  // DefineButton2
-	    ($buttonHasBlendMode == 1)) {
-	    $buttonrecord['BlendMode'] = $reader->getUI8();
+        //
+        $buttonrecord['CharacterID'] = $reader->getUI16LE();
+        $buttonrecord['PlaceDepth'] = $reader->getUI16LE();
+        $buttonrecord['PlaceMatrix'] = IO_SWF_Type_MATRIX::parse($reader);
+        if ($opts['tagCode'] == 34) { // DefineButton2
+            $buttonrecord['ColorTransform'] = IO_SWF_Type_CXFORMWITHALPHA::parse($reader);
+        } else {
+            $buttonrecord['ColorTransform'] = IO_SWF_Type_CXFORM::parse($reader);
+        }
+        if (($opts['tagCode'] == 34) &&  // DefineButton2
+            ($buttonHasFilterList == 1)) {
+            $buttonrecord['FilterList'] = IO_SWF_Type_FILTERLIST::parse($reader);
+        }
+        if (($opts['tagCode'] == 34) &&  // DefineButton2
+            ($buttonHasBlendMode == 1)) {
+            $buttonrecord['BlendMode'] = $reader->getUI8();
         }
     	return $buttonrecord;
     }
-    static function build(&$writer, $cxform, $opts = array()) {
-        $nbits = 0;
-        $hasAddTerms = 0;
-        $hasMultiTerms = 0;
-        $multi_term_list = array('RedMultiTerm', 'GreenMultiTerm', 'BlueMultiTerm');
-        foreach ($multi_term_list as $term) {
-            if (isset($cxform[$term])) {
-                $hasMultiTerms = 1;
-                $need_bits = $writer->need_bits_signed($cxform[$term]);
-                if ($nbits < $need_bits){
-                    $nbits = $need_bits;
-                }
-	    }
-	}
-	$add_term_list = array('RedAddTerm', 'GreenAddTerm', 'BlueAddTerm');
-	foreach ($add_term_list as $term) {
-	    if (isset($cxform[$term])) {
-	        $hasAddTerms = 1;
-            $need_bits = $writer->need_bits_signed($cxform[$term]);
-            if ($nbits < $need_bits){
-                $nbits = $need_bits;
-            }
-	    }
-	}
-	$writer->putUIBit($hasAddTerms);
-	$writer->putUIBit($hasMultiTerms);
-	$writer->putUIBits($nbits, 4);
-	if ($hasMultiTerms) {
-	    $writer->putSIBits($cxform['RedMultiTerm'],   $nbits);
-	    $writer->putSIBits($cxform['GreenMultiTerm'], $nbits);
-	    $writer->putSIBits($cxform['BlueMultiTerm'],  $nbits);
-	}
-	if ($hasAddTerms) {
-	  $writer->putSIBits($cxform['RedAddTerm'],   $nbits);
-	  $writer->putSIBits($cxform['GreenAddTerm'], $nbits);
-	  $writer->putSIBits($cxform['BlueAddTerm'],  $nbits);
-	}
+
+    static function build(&$writer, $buttonrecord, $opts = array()) {
+        $writer->putUIBits(0, 2); // ButtonReserved
+        $buttonHasBlendMode = $buttonrecord['ButtonHasBlandMode'];
+        $buttonHasFilterList = $buttonrecord['ButtonHasFilterList'];
+        $writer->putUIBit($buttonHasBlendMode);
+        $writer->putUIBit($buttonHasFilterList);
+        $writer->putUIBit($buttonrecord['ButtonStateHitTest']);
+        $writer->putUIBit($buttonrecord['ButtonStateDown']);
+        $writer->putUIBit($buttonrecord['ButtonStateOver']);
+        $writer->putUIBit($buttonrecord['ButtonStateUp']);
+        //
+        $writer->putUI16LE($buttonrecord['CharacterID']);
+        $reader->putUI16LE($buttonrecord['PlaceDepth']);
+        IO_SWF_Type_MATRIX::build($writer, $buttonrecord['PlaceMatrix']);
+        if ($opts['tagCode'] == 34) { // DefineButton2
+            IO_SWF_Type_CXFORMWITHALPHA::build($writer, $buttonrecord['ColorTransform']);
+        } else {
+            IO_SWF_Type_CXFORM::build($writer, $buttonrecord['ColorTransform']);
+        }
+        if (($opts['tagCode'] == 34) &&  // DefineButton2
+            ($buttonHasFilterList == 1)) {
+            IO_SWF_Type_FILTERLIST::build($writer, $buttonrecord['FilterList']);
+        }
+        if (($opts['tagCode'] == 34) &&  // DefineButton2
+            ($buttonHasBlendMode == 1)) {
+            $writer->putUI8($buttonrecord['BlendMode']);
+        }
     }
-    static function string($cxform, $opts = array()) {
-        if (($cxform['HasMultiTerms'] == 0) && ($cxform['HasAddTerms'] == 0)) {
-            return '(No Data: CXFORM)';
-        }
+    static function string($buttonrecord, $opts = array()) {
         $text = '';
-        if ($cxform['HasMultiTerms']) {
-            $text .= sprintf("MultiTerms:(%d,%d,%d)", $cxform['RedMultiTerm'], $cxform['GreenMultiTerm'], $cxform['BlueMultiTerm']);
+        $buttonHasBlendMode = $buttonrecord['ButtonHasBlandMode'];
+        $buttonHasFilterList = $buttonrecord['ButtonHasFilterList'];
+        echo 'ButtonHasBlandMode:'.$buttonrecord['ButtonHasBlandMode'].' ButtonHasFilterList:'. $buttonrecord['ButtonHasFilterList']."\n";
+        foreach (array('ButtonStateHitTest', 'ButtonStateDown', 'ButtonStateOver', 'ButtonStateUp') as $label) {
+            $text .= $label.':'.$buttonrecord[$label].' ';
         }
-        if ($cxform['HasAddTerms']) {
-            if ($cxform['HasMultiTerms']) {
-                $text .= ' ';
-            }
-            $text .= sprintf("AddTerms:(%d,%d,%d)", $cxform['RedAddTerm'], $cxform['GreenAddTerm'], $cxform['BlueAddTerm']);
+        $text .= "\n";
+        $text .= IO_SWF_Type_MATRIX::string($buttonrecord['PlaceMatrix']);
+        if ($opts['tagCode'] == 34) { // DefineButton2
+            $text .= 'ColorTransform:'. IO_SWF_Type_CXFORMWITHALPHA::string($buttonrecord['ColorTransform']).' ';
+        } else {
+            $text .= 'ColorTransform:'.IO_SWF_Type_CXFORM::string($buttonrecord['ColorTransform']).' ';
+        }
+        if (($opts['tagCode'] == 34) &&  // DefineButton2
+            ($buttonHasFilterList == 1)) {
+            $text .= 'FilterList:'.IO_SWF_Type_FILTERLIST::string($buttonrecord['FilterList'])."\n";
+        }
+        if (($opts['tagCode'] == 34) &&  // DefineButton2
+            ($buttonHasBlendMode == 1)) {
+            $text .= 'BlendMode:'.$buttonrecord['BlendMode']."\n";
         }
         return $text;
     }
