10d65999a7512e511616e5d80ede295174680bbd
よや <yoya@awm.jp>
2011-12-05 15:38:02 +0000

    CID 取得 function Font 系タグ対応

--- IO/SWF/Editor.php
+++ IO/SWF/Editor.php
@@ -53,6 +53,9 @@ class IO_SWF_Editor extends IO_SWF {
               case 22: // DefineShape2 (ShapeId)
               case 32: // DefineShape3 (ShapeId)
               case 46: // DefineMorphShape (ShapeId)
+              case 10: // DefineFont
+              case 48: // DefineFont2
+              case 75: // DefineFont3
               case 11: // DefineText
               case 33: // DefineText2
               case 37: // DefineTextEdit
@@ -74,6 +77,8 @@ class IO_SWF_Editor extends IO_SWF {
             switch ($tag->code) {
               case 4:  // PlaceObject
               case 5:  // RemoveObject
+              case 13:  // DefineFontInfo
+              case 73:  // DefineFontAlignZones
                 $tag->referenceId = $content_reader->getUI16LE();
                 break;
               case 26: // PlaceObject2 (Shape Reference)
