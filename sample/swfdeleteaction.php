ead8fb044719c84232d60464284be01ccb974bd4
よや <yoya@awm.jp>
2012-02-05 14:52:37 +0000

    usage text typo

--- sample/swfdeleteaction.php
+++ sample/swfdeleteaction.php
@@ -3,8 +3,8 @@
 require 'IO/SWF/Editor.php';
 
 if ($argc != 2) {
-    echo "Usage: php swfrebuild.php <swf_file>\n";
-    echo "ex) php swfrebuild.php test.swf\n";
+    echo "Usage: php swfdeleteaction.php <swf_file>\n";
+    echo "ex) php swfdeleteaction.php test.swf\n";
     exit(1);
 }
 
