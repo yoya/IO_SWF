52b1c725256052ca5f9c4df4158e61ec028ae3ba
よや <yoya@awm.jp>
2012-02-05 03:39:54 +0000

    Actionタグ削除ツール

---
+++ sample/swfdeleteaction.php
@@ -0,0 +1,28 @@
+<?php
+
+require 'IO/SWF/Editor.php';
+
+if ($argc != 2) {
+    echo "Usage: php swfrebuild.php <swf_file>\n";
+    echo "ex) php swfrebuild.php test.swf\n";
+    exit(1);
+}
+
+assert(is_readable($argv[1]));
+
+$swfdata = file_get_contents($argv[1]);
+
+$swf = new IO_SWF_Editor();
+
+$swf->parse($swfdata);
+
+foreach ($swf->_tags as $idx => $tag) {
+    $tag_code = $tag->code;
+    if (($tag_code == 12) || ($tag_code == 59)) { // DoAction, DoInitAction
+        unset($swf->_tags[$idx]);
+    }
+}
+
+echo $swf->build();
+
+exit(0);
