6e5741d4cb9c37d8fa2e199837e372fcae7a349e
よや <yoya@awm.jp>
2012-02-20 14:07:34 +0000

    Delete ActionTag in DefineSprite.

--- sample/swfdeleteaction.php
+++ sample/swfdeleteaction.php
@@ -16,10 +16,25 @@ $swf = new IO_SWF_Editor();
 
 $swf->parse($swfdata);
 
-foreach ($swf->_tags as $idx => $tag) {
+foreach ($swf->_tags as $idx => &$tag) {
     $tag_code = $tag->code;
     if (($tag_code == 12) || ($tag_code == 59)) { // DoAction, DoInitAction
         unset($swf->_tags[$idx]);
+    }
+    if ($tag_code == 39) { // DefineSprite
+        if ($tag->parseTagContent() === false) {
+            echo "Unknown DefineSprite!!!\n";
+            exit(1);
+        }
+        foreach ($tag->tag->_controlTags as $idx_in_sprite => &$tag_in_sprite) {
+            $tag_code_in_sprite = $tag_in_sprite->code;
+            // DoAction, DoInitAction
+            if (($tag_code_in_sprite == 12) || ($tag_code_in_sprite == 59)) {
+                unset($tag->tag->_controlTags->_tags[$idx_in_sprite]);
+                $tag_in_sprite->content = null;
+                $tag->content = null;
+            }
+        }
     }
 }
 
