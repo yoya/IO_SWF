65faf2c60eda948118f5935b6874a1aaf37862b3
よや <yoya@awm.jp>
2012-11-02 12:03:03 +0000

    moving $tagMap function to class scope.

--- IO/SWF/Tag.php
+++ IO/SWF/Tag.php
@@ -9,93 +9,93 @@ class IO_SWF_Tag {
     var $content = null;
     var $tag = null;
     var $byte_offset, $byte_size;
+    static $tagMap = array(
+        // code => array(name , klass)
+        0 => array('name' => 'End'),
+        1 => array('name' => 'ShowFrame'),
+        2 => array('name' => 'DefineShape',  'klass' => 'Shape'),
+//             3 => array('name' => 'FreeCharacter'), // ???
+        4 => array('name' => 'PlaceObject', 'klass' => 'Place'),
+        5 => array('name' => 'RemoveObject', 'klass' => 'Remove'),
+        6 => array('name' => 'DefineBits', 'klass' => 'Jpeg'),
+        7 => array('name' => 'DefineButton', 'klass' => 'Button'),
+        8 => array('name' => 'JPEGTables', 'klass' => 'Jpeg'),
+        9 => array('name' => 'SetBackgroundColor', 'klass' => 'BGColor'),
+        10 => array('name' => 'DefineFont'),
+        11 => array('name' => 'DefineText', 'klass' => 'Text'),
+        12 => array('name' => 'DoAction', 'klass' => 'Action'),
+        13 => array('name' => 'DefineFontInfo'),
+        14 => array('name' => 'DefineSound'),
+        15 => array('name' => 'StartSound'),
+        // 16 missing
+        17 => array('name' => 'DefineButtonSound'),
+        18 => array('name' => 'SoundStreamHead'),
+        19 => array('name' => 'SoundStreamBlock'),
+        20 => array('name' => 'DefineBitsLossless', 'klass' => 'Lossless'),
+        21 => array('name' => 'DefineBitsJPEG2', 'klass' => 'Jpeg'),
+        22 => array('name' => 'DefineShape2', 'klass' => 'Shape'),
+        24 => array('name' => 'Protect'),
+        // 25 missing
+        26 => array('name' => 'PlaceObject2', 'klass' => 'Place'),
+        // 27 missing
+        28 => array('name' => 'RemoveObject2', 'klass' => 'Remove'),
+        // 29,30,31 missing
+        32 => array('name' => 'DefineShape3', 'klass' => 'Shape'),
+        33 => array('name' => 'DefineText2', 'klass' => 'Text'),
+        34 => array('name' => 'DefineButton2', 'klass' => 'Button'),
+        35 => array('name' => 'DefineBitsJPEG3', 'klass' => 'Jpeg'),
+        36 => array('name' => 'DefineBitsLossless2', 'klass' => 'Lossless'),
+        37 => array('name' => 'DefineEditText', 'klass' => 'EditText'),
+        // 38 missing
+        39 => array('name' => 'DefineSprite', 'klass' => 'Sprite'),
+        // 40,41,42 missing
+        43 => array('name' => 'FrameLabel', 'klass' => 'FrameLabel'),
+        // 44 missing
+        45 => array('name' => 'SoundStreamHead2'),
+        46 => array('name' => 'DefineMorphShape', 'klass' => 'Shape'),
+        48 => array('name' => 'DefineFont2', 'klass' => 'Font'),
+        56 => array('name' => 'Export'),
+        57 => array('name' => ''),
+        58 => array('name' => ''),
+        59 => array('name' => 'DoInitAction', 'klass' => 'Action'),
+        //
+        60 => array('name' => 'DefineVideoStream'),
+        61 => array('name' => 'videoFrame'),
+        62 => array('name' => 'DefineFontInfo2'),
+        // 63 missing
+        64 => array('name' => 'EnableDebugger2'),
+        65 => array('name' => 'ScriptLimits'),
+        66 => array('name' => 'SetTabIndex'),
+        // 67,68 missing 
+        69 => array('name' => 'FileAttributes'),
+        70 => array('name' => 'PlaceObject3'),
+        71 => array('name' => 'ImportAssets2'),
+        // 72 missing
+        73 => array('name' => 'DefineFontAlignZones'),
+        74 => array('name' => 'CSMTextSettings'),
+        75 => array('name' => 'DefineFont3'),
+        76 => array('name' => 'SymbolClass'),
+        77 => array('name' => 'MetaData'),
+        78 => array('name' => 'DefineScalingGrid'),
+        // 79,80,81 missing
+        82 => array('name' => 'DoABC'),
+        83 => array('name' => 'DefineShape4', 'klass' => 'Shape'),
+        84 => array('name' => 'DefineMorphShape2'),
+        // 85 missing
+        86 => array('name' => 'DefineSceneAndFrameLabelData'),
+        87 => array('name' => 'DefineBinaryData'),
+        88 => array('name' => 'DefineFontName'),
+        89 => array('name' => 'StartSound2'),
+        90 => array('name' => 'DefineBitsJPEG4'),
+        91 => array('name' => 'DefineFont4'),
+        777 => array('name' => 'Reflex'), // swftools ?
+        );
     function __construct($swfInfo) {
         $this->swfInfo = $swfInfo;
     }
     function getTagInfo($tagCode, $label) {
-        static $tagMap = array(
-         // code => array(name , klass)
-             0 => array('name' => 'End'),
-             1 => array('name' => 'ShowFrame'),
-             2 => array('name' => 'DefineShape',  'klass' => 'Shape'),
-//             3 => array('name' => 'FreeCharacter'), // ???
-             4 => array('name' => 'PlaceObject', 'klass' => 'Place'),
-             5 => array('name' => 'RemoveObject', 'klass' => 'Remove'),
-             6 => array('name' => 'DefineBits', 'klass' => 'Jpeg'),
-             7 => array('name' => 'DefineButton', 'klass' => 'Button'),
-             8 => array('name' => 'JPEGTables', 'klass' => 'Jpeg'),
-             9 => array('name' => 'SetBackgroundColor', 'klass' => 'BGColor'),
-            10 => array('name' => 'DefineFont'),
-            11 => array('name' => 'DefineText', 'klass' => 'Text'),
-            12 => array('name' => 'DoAction', 'klass' => 'Action'),
-            13 => array('name' => 'DefineFontInfo'),
-            14 => array('name' => 'DefineSound'),
-            15 => array('name' => 'StartSound'),
-            // 16 missing
-            17 => array('name' => 'DefineButtonSound'),
-            18 => array('name' => 'SoundStreamHead'),
-            19 => array('name' => 'SoundStreamBlock'),
-            20 => array('name' => 'DefineBitsLossless', 'klass' => 'Lossless'),
-            21 => array('name' => 'DefineBitsJPEG2', 'klass' => 'Jpeg'),
-            22 => array('name' => 'DefineShape2', 'klass' => 'Shape'),
-            24 => array('name' => 'Protect'),
-            // 25 missing
-            26 => array('name' => 'PlaceObject2', 'klass' => 'Place'),
-            // 27 missing
-            28 => array('name' => 'RemoveObject2', 'klass' => 'Remove'),
-            // 29,30,31 missing
-            32 => array('name' => 'DefineShape3', 'klass' => 'Shape'),
-            33 => array('name' => 'DefineText2', 'klass' => 'Text'),
-            34 => array('name' => 'DefineButton2', 'klass' => 'Button'),
-            35 => array('name' => 'DefineBitsJPEG3', 'klass' => 'Jpeg'),
-            36 => array('name' => 'DefineBitsLossless2', 'klass' => 'Lossless'),
-            37 => array('name' => 'DefineEditText', 'klass' => 'EditText'),
-            // 38 missing
-            39 => array('name' => 'DefineSprite', 'klass' => 'Sprite'),
-            // 40,41,42 missing
-            43 => array('name' => 'FrameLabel', 'klass' => 'FrameLabel'),
-            // 44 missing
-            45 => array('name' => 'SoundStreamHead2'),
-            46 => array('name' => 'DefineMorphShape', 'klass' => 'Shape'),
-            48 => array('name' => 'DefineFont2', 'klass' => 'Font'),
-            56 => array('name' => 'Export'),
-            57 => array('name' => ''),
-            58 => array('name' => ''),
-            59 => array('name' => 'DoInitAction', 'klass' => 'Action'),
-            //
-            60 => array('name' => 'DefineVideoStream'),
-            61 => array('name' => 'videoFrame'),
-            62 => array('name' => 'DefineFontInfo2'),
-            // 63 missing
-            64 => array('name' => 'EnableDebugger2'),
-            65 => array('name' => 'ScriptLimits'),
-            66 => array('name' => 'SetTabIndex'),
-            // 67,68 missing 
-            69 => array('name' => 'FileAttributes'),
-            70 => array('name' => 'PlaceObject3'),
-            71 => array('name' => 'ImportAssets2'),
-            // 72 missing
-            73 => array('name' => 'DefineFontAlignZones'),
-            74 => array('name' => 'CSMTextSettings'),
-            75 => array('name' => 'DefineFont3'),
-            76 => array('name' => 'SymbolClass'),
-            77 => array('name' => 'MetaData'),
-            78 => array('name' => 'DefineScalingGrid'),
-            // 79,80,81 missing
-            82 => array('name' => 'DoABC'),
-            83 => array('name' => 'DefineShape4', 'klass' => 'Shape'),
-            84 => array('name' => 'DefineMorphShape2'),
-            // 85 missing
-            86 => array('name' => 'DefineSceneAndFrameLabelData'),
-            87 => array('name' => 'DefineBinaryData'),
-            88 => array('name' => 'DefineFontName'),
-            89 => array('name' => 'StartSound2'),
-            90 => array('name' => 'DefineBitsJPEG4'),
-            91 => array('name' => 'DefineFont4'),
-            777 => array('name' => 'Reflex'), // swftools ?
-        );
-        if (isset($tagMap[$tagCode][$label])) {
-           return $tagMap[$tagCode][$label];
+        if (isset(self::$tagMap[$tagCode][$label])) {
+           return self::$tagMap[$tagCode][$label];
         }
         return false;
     }
