131025736a777aa1e3f18f1b206c428f99e3275f
よや <yoya@awm.jp>
2012-11-12 13:44:14 +0000

    add options  -f (swf file)  -a (all)  -s (sprite select)  -l (length select)

--- sample/swfdeleteaction.php
+++ sample/swfdeleteaction.php
@@ -2,36 +2,70 @@
 
 require 'IO/SWF/Editor.php';
 
-if ($argc != 2) {
-    echo "Usage: php swfdeleteaction.php <swf_file>\n";
-    echo "ex) php swfdeleteaction.php test.swf\n";
+$options = getopt("f:as:l:");
+
+if ((isset($options['f']) === false) ||
+    (is_readable($options['f']) === false)) {
+    echo "Usage: php swfdeleteaction.php -f <swf_file> [-a] [-s <sprite_id>[:<frame>] -l [<length>]\n";
+    echo "ex) php swfdeleteaction.php -f test.swf -a\n";
+    echo "ex) php swfdeleteaction.php -f test.swf -s 130 -f 20\n";
     exit(1);
 }
+$all = isset($options['a'])?true:false;
+if (isset($options['s'])) {
+    $spriteId_frameNum = explode(":", $options['s']);
+    if (count($spriteId_frameNum) === 1) {
+        $spriteId = $spriteId_frameNum[0];
+        $frameNum = null;
+    } else {
+        list($spriteId, $frameNum) = $spriteId_frameNum;
+    }
+} else {
+    $spriteId = null;
+    $frameNum = null;
+}
+$length = isset($options['l'])?$options['l']:null;
 
-assert(is_readable($argv[1]));
-
-$swfdata = file_get_contents($argv[1]);
+$swfdata = file_get_contents($options['f']);
 
 $swf = new IO_SWF_Editor();
 
 $swf->parse($swfdata);
 
+$currentFrame = 1;
 foreach ($swf->_tags as $idx => &$tag) {
     $tag_code = $tag->code;
+    if ($tag_code == 1) { // ShowFrame
+        $currentFrame++;
+        continue;
+    }
     if (($tag_code == 12) || ($tag_code == 59)) { // DoAction, DoInitAction
-        unset($swf->_tags[$idx]);
+        if ($all ||
+            ((is_null($spriteId) === false) && ($spriteId == 0) && (is_null($frameNum) || ($frameNum == $currentFrame))) ||
+            ($length == strlen($tag->content))) {
+            unset($swf->_tags[$idx]);
+        }
     }
     if ($tag_code == 39) { // DefineSprite
         if ($tag->parseTagContent() === false) {
             echo "Unknown DefineSprite!!!\n";
             exit(1);
         }
+        $currentFrameInSprite = 1;
         foreach ($tag->tag->_controlTags as $idx_in_sprite => &$tag_in_sprite) {
             $tag_code_in_sprite = $tag_in_sprite->code;
+            if ($tag_code_in_sprite == 1) { // ShowFrame
+                $currentFrameInSprite++;
+                continue;
+            }
             // DoAction, DoInitAction
             if (($tag_code_in_sprite == 12) || ($tag_code_in_sprite == 59)) {
+                if ($all ||
+                    ((is_null($spriteId) === false) && ($spriteId == $tag->tag->_spriteId) && (is_null($frameNum) || ($frameNum == $currentFrameInSprite))) ||
+                    ($length == strlen($tag_in_sprite->content))) {
                 unset($tag->tag->_controlTags[$idx_in_sprite]);
-                $tag->content = null;
+                    $tag->content = null;
+                }
             }
         }
     }
