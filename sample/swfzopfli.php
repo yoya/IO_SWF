9b6148da70dbb43b0ee0a4ecb541269bf83212b1
よや <yoya@awm.jp>
2013-04-22 14:08:40 +0000

    Zopfli で画像タグの Zlib を再圧縮するツール

---
+++ sample/swfzopfli.php
@@ -0,0 +1,58 @@
+<?php
+// require => https://github.com/kjdev/php-ext-zopfli
+
+require 'IO/SWF/Editor.php';
+
+function zopfli_recompress($d) {
+    $u = zopfli_uncompress($d);
+    return zopfli_compress($u, 100);
+}
+
+function swftag_recompress(&$tag) {
+    $ret = false;
+    $zlibProperties = Array('_ZlibBitmapData', '_ZlibBitmapAlphaData');
+    foreach ($zlibProperties as $methodName) {
+        if (isset($tag->$methodName)) {
+            if (strlen($tag->$methodName) > 0) {
+                $z = zopfli_recompress($tag->$methodName);
+                if ($z !== false) {
+                    $tag->$methodName = $z;
+                    $ret = true;
+                }
+            }
+        }
+    }
+    return $ret;
+}
+
+if ($argc != 2) {
+    echo "Usage: php swfzopfli.php <swf_file>\n";
+    echo "ex) php swfzopfli.php test.swf\n";
+    exit(1);
+}
+
+assert(is_readable($argv[1]));
+
+$swfdata = file_get_contents($argv[1]);
+
+$swf = new IO_SWF_Editor();
+
+$swf->parse($swfdata);
+
+foreach ($swf->_tags as $idx => &$tag) {
+    switch ($tag->code) {
+    case 26: // DefineLossless
+    case 35: // DefineBitsJPEG3
+    case 36: // DefineLossless2
+        if ($tag->parseTagContent()) {
+            $ret = swftag_recompress($tag->tag);
+            if ($ret) {
+                $tag->content = null;
+            }
+        }
+    }
+}
+
+echo $swf->build();
+
+exit(0);
