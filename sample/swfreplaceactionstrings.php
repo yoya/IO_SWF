db22435d2bc731bdba4d3612d7c91b729f69ac3f
よや <yoya@awm.jp>
2015-06-30 05:55:47 +0000

    - 複数の入れ替えが同時にできるように - -f の標準入力対応。

--- sample/swfreplaceactionstrings.php
+++ sample/swfreplaceactionstrings.php
@@ -3,25 +3,29 @@
 require 'IO/SWF/Editor.php';
 // require dirname(__FILE__).'/../IO/SWF/Editor.php';
 
-if ($argc != 4) {
-    fprintf(STDERR, "Usage: php swfreplaceactionstrings.php <swf_file> <from_str> <to_str>\n");
+if ( ($argc < 4) || (($argc % 2) == 0) ||
+      (($argv[1] !== '-') && (is_readable($argv[1]) === false)) ) {
+    fprintf(STDERR, "Usage: php swfreplaceactionstrings.php <swf_file> <from_str> <to_str> [<from_str2> <to_str2> [...]]\n");
     fprintf(STDERR, "ex) php swfreplaceactionstrings.php test.swf foo baa\n");
     exit(1);
 }
 
-assert(is_readable($argv[1]));
-assert(isset($argv[2]));
-assert(isset($argv[3]));
+if ($argv[1] === '-') {
+  $swfdata = "php://stdin";
+} else {
+  $swfdata = file_get_contents($argv[1]);
+}
 
-$swfdata = file_get_contents($argv[1]);
-$from_str = $argv[2];
-$to_str = $argv[3];
+$trans_table = array();
+for ($i = 2 ; $i < $argc ; $i+=2) {
+    $trans_table[$argv[$i]] = $argv[$i+1];
+}
 
 $swf = new IO_SWF_Editor();
 
 $swf->parse($swfdata);
 
-$swf->replaceActionStrings($from_str, $to_str);
+$swf->replaceActionStrings($trans_table);
 
 echo $swf->build();
 
