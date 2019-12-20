14dbffbc3d863de4a6e152c005aa3ff7373dfe14
Yoshihiro Yamazaki <yoya@awm.jp>
2019-12-20 10:14:49 +0900

    dump shape rectangle with matrix multiply

--- sample/swftreematrix.php
+++ sample/swftreematrix.php
@@ -65,7 +65,7 @@ function swftreematrix($swf, $tags, $parentMatrix, $indent) {
             }
             echo "$name\n";
             $multipliedMatrix = multiplyMatrix($matrix, $parentMatrix);
-            dumpMatrix([$matrix, $parentMatrix, $multipliedMatrix], 1).PHP_EOL;
+            dumpMatrix([$matrix, $parentMatrix, null, $multipliedMatrix], $indent+0.5);
             $target_tag = $swf->getTagByCharacterId($cid);
             swftreematrix($swf, [$target_tag], $multipliedMatrix, $indent+1);
             break;
@@ -80,7 +80,9 @@ function swftreematrix($swf, $tags, $parentMatrix, $indent) {
             if (! $tag->parseTagContent()) {
                 throw Exception("can't parse sprite tag content");
             }
-            // var_dump($tag->tag->_shapeBounds);
+            $rect = $tag->tag->_shapeBounds;
+            $multipliedRect = multiplyMatrix($parentMatrix, $rect);
+            dumpMatrix([$parentMatrix, $rect, null, $multipliedRect], $indent+0.5).PHP_EOL;
             break;
         }
     }
@@ -88,16 +90,29 @@ function swftreematrix($swf, $tags, $parentMatrix, $indent) {
 
 function dumpMatrix($matrixArray, $indent) {
     $indentStr = str_repeat(" ", 4*$indent);
-    $text_fmt = " %3.3f %3.3f %3.2f |";
-    $indentStr = str_repeat(" ", 4*$indent - 1);
+    $text_fmt3 = " %3.3f %3.3f %3.2f |";
+    $text_fmt2 = " %3.3f %3.3f |";
+    $indentStr = str_repeat(" ", 4*$indent);
     $text1 = $text2 = $indentStr."|";
     foreach ($matrixArray as $matrix) {
-        $text1 .= sprintf($text_fmt, $matrix['ScaleX'] / 0x10000,
-                          $matrix['RotateSkew0'] / 0x10000,
-                          $matrix['TranslateX'] / 20);
-        $text2 .= sprintf($text_fmt, $matrix['RotateSkew1'] / 0x10000,
-                          $matrix['ScaleY'] / 0x10000,
-                          $matrix['TranslateY'] / 20);
+        if (isset($matrix['ScaleX'])) {
+            $text1 .= sprintf($text_fmt3, $matrix['ScaleX'] / 0x10000,
+                              $matrix['RotateSkew0'] / 0x10000,
+                              $matrix['TranslateX'] / 20);
+            $text2 .= sprintf($text_fmt3, $matrix['RotateSkew1'] / 0x10000,
+                              $matrix['ScaleY'] / 0x10000,
+                              $matrix['TranslateY'] / 20);
+        } else if (isset($matrix['Xmin'])) {
+            $text1 .= sprintf($text_fmt2,
+                              $matrix['Xmin'] / 20, $matrix['Xmax'] / 20);
+            $text2 .= sprintf($text_fmt2,
+                              $matrix['Ymin'] / 20, $matrix['Ymax'] / 20);
+        } else if ($matrix == null) {
+            $text1 .= " _ |";
+            $text2 .= " ~ |";
+        } else {
+            throw new Exception("dumpMatrix: unknown value");
+        }
     }
     echo $text1.PHP_EOL.$text2.PHP_EOL;
 }
@@ -115,27 +130,52 @@ function identMatrix() {
 }
 
 function multiplyMatrix($mat1, $mat2) {
-    $matrix = [
-        'ScaleX' => (
-            $mat1['ScaleX'] * $mat2['ScaleX'] / 0x10000 +
-            $mat1['RotateSkew0'] * $mat2['RotateSkew1'] / 0x10000 ),
-        'RotateSkew0' => (
-            $mat1['ScaleX'] * $mat2['RotateSkew0'] / 0x10000 +
-            $mat1['RotateSkew0'] * $mat2['ScaleY'] / 0x10000 ),
-        'TranslateX' => (
-            $mat1['ScaleX'] * $mat2['TranslateX']  / 0x10000 / 20 +
-            $mat1['RotateSkew0'] * $mat2['TranslateY'] / 0x10000 / 20 +
-            $mat1['TranslateX'] ),
-        'RotateSkew1' => (
-            $mat1['RotateSkew1'] * $mat2['ScaleX'] / 0x10000 +
-            $mat1['ScaleY'] * $mat2['RotateSkew1'] / 0x10000 ),
-        'ScaleY' => (
-            $mat1['RotateSkew1'] * $mat2['RotateSkew0'] / 0x10000+
-            $mat1['ScaleY'] * $mat2['ScaleY'] / 0x10000 ),
-        'TranslateY' => (
-            $mat1['RotateSkew1'] * $mat2['TranslateX'] / 0x10000 / 20 +
-            $mat1['ScaleY'] * $mat2['TranslateY']  / 0x10000 / 20 +
-            $mat1['TranslateY'] ),
-    ];
-    return $matrix;
+    if (isset($mat1['ScaleX']) && isset($mat2['ScaleX'])) {
+        // matrix x matrix
+        $ret = [
+            'ScaleX' => (
+                $mat1['ScaleX']      * $mat2['ScaleX']      / 0x10000 +
+                $mat1['RotateSkew0'] * $mat2['RotateSkew1'] / 0x10000 ),
+            'RotateSkew0' => (
+                $mat1['ScaleX']      * $mat2['RotateSkew0'] / 0x10000 +
+                $mat1['RotateSkew0'] * $mat2['ScaleY']      / 0x10000 ),
+            'TranslateX' => (
+                $mat1['ScaleX']      * $mat2['TranslateX'] / 0x10000 / 20 +
+                $mat1['RotateSkew0'] * $mat2['TranslateY'] / 0x10000 / 20 +
+                $mat1['TranslateX'] ),
+            'RotateSkew1' => (
+                $mat1['RotateSkew1'] * $mat2['ScaleX']      / 0x10000 +
+                $mat1['ScaleY']      * $mat2['RotateSkew1'] / 0x10000 ),
+            'ScaleY' => (
+                $mat1['RotateSkew1'] * $mat2['RotateSkew0'] / 0x10000 +
+                $mat1['ScaleY']      * $mat2['ScaleY']      / 0x10000 ),
+            'TranslateY' => (
+                $mat1['RotateSkew1'] * $mat2['TranslateX'] / 0x10000 / 20 +
+                $mat1['ScaleY']      * $mat2['TranslateY'] / 0x10000 / 20 +
+                $mat1['TranslateY'] ),
+        ];
+    } else if (isset($mat2["Xmin"])) {
+        // matrix x rect
+        $ret = [
+            'Xmin' => (
+                $mat1['ScaleX']      * $mat2['Xmin'] / 0x10000 +
+                $mat1['RotateSkew0'] * $mat2['Ymin'] / 0x10000 +
+                $mat1['TranslateX'] ),
+            'Ymin' => (
+                $mat1['RotateSkew1'] * $mat2['Xmin'] / 0x10000 +
+                $mat1['ScaleY']      * $mat2['Ymin'] / 0x10000 +
+                $mat1['TranslateY'] ),
+            'Xmax' => (
+                $mat1['ScaleX']      * $mat2['Xmin'] / 0x10000 +
+                $mat1['RotateSkew0'] * $mat2['Ymin'] / 0x10000 +
+                $mat1['TranslateX'] ),
+            'Ymax' => (
+                $mat1['RotateSkew1'] * $mat2['Xmax'] / 0x10000 +
+                $mat1['ScaleY']      * $mat2['Ymax'] / 0x10000 +
+                $mat1['TranslateY'] ),
+        ];
+    } else {
+        throw new Exception("multiplyMatrix: unknown value");
+    }
+    return $ret;
 }
