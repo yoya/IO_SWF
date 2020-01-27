678b4651c7614b2c09a13c75b64eda6b1a02a0a9
Yoshihiro Yamazaki <yoya@awm.jp>
2020-01-27 22:34:43 +0900

    rename instance variables $this->symbols => $this->Symbols.

--- IO/SWF/Tag/SymbolClass.php
+++ IO/SWF/Tag/SymbolClass.php
@@ -5,24 +5,24 @@ require_once dirname(__FILE__).'/Base.php';
 require_once dirname(__FILE__).'/../Type/String.php';
 
 class IO_SWF_Tag_SymbolClass extends IO_SWF_Tag_Base {
-    var $_symbols;
+    var $_Symbols;
 
     function parseContent($tagCode, $content, $opts = array()) {
         $reader = new IO_Bit();
     	$reader->input($content);
         $numSymbols = $reader->getUI16LE();
-        $this->_symbols = [];
+        $this->_Symbols = [];
         for ($i = 0; $i < $numSymbols; $i++) {
             $tag = $reader->getUI16LE();
             $name = IO_SWF_Type_String::parse($reader);
-            $this->_symbols [] = ["Tag" => $tag, "Name" => $name];
+            $this->_Symbols [] = ["Tag" => $tag, "Name" => $name];
         }
     }
 
     function dumpContent($tagCode, $opts = array()) {
-        $numSymbols = count($this->_symbols);
+        $numSymbols = count($this->_Symbols);
         echo "\tSymbols (count:$numSymbols)\n";
-        foreach ($this->_symbols as $idx => $symbol) {
+        foreach ($this->_Symbols as $idx => $symbol) {
             $tag = $symbol["Tag"];
             $name = $symbol["Name"];
             echo "\t[$idx] Tag: $tag  name: $name\n";
@@ -31,9 +31,9 @@ class IO_SWF_Tag_SymbolClass extends IO_SWF_Tag_Base {
 
     function buildContent($tagCode, $opts = array()) {
         $writer = new IO_Bit();
-        $numSymbols = count($this->_symbols);
+        $numSymbols = count($this->_Symbols);
         $writer->putUI16LE($numSymbols);
-        foreach ($this->_symbols as $symbol) {
+        foreach ($this->_Symbols as $symbol) {
             $writer->putUI16LE($symbol["Tag"]);
             IO_SWF_Type_String::build($writer, $symbol["Name"]);
         }
