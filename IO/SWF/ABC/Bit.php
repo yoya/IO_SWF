012db8022b1c9152ec4828a79bc78fa9536a93e9
Yoshihiro Yamazaki <yoya@awm.jp>
2020-02-01 04:09:05 +0900

    fix s24, s30, s32 parser.

--- IO/SWF/ABC/Bit.php
+++ IO/SWF/ABC/Bit.php
@@ -17,24 +17,26 @@ class IO_SWF_ABC_Bit extends IO_Bit {
         return $this->getUI8();
     }
     function get_u16() {
-        return $this->getUI16BE(16);
+        return $this->getUI16LE();
     }
     function get_s24() {
-        $this->byteAlign();
-        return $this->getUIBits(24);
+        $v = $this->getUI16LE();
+        $v = ($this->getUI8() << 16) + $v;
+        return ($v < 0x800000)? $v: ($v - 0x1000000); // 2-negative
     }
-    function get_u30() {  // XXX
+    function get_u30() {
         $v = $this->get_u32();
         if ($v > 0x3FFFFFFF) {
             throw new IO_SWF_Exception("Error: get_u30() v:$v > 0x3FFFFFFF");
         }
+        return $v;
     }
     function get_u32() {
         $v = 0; $s = 0; $i = 0;
         for ($i = 0; $i < 5; $i++) {
             $b = $this->getUI8();
             $v |= ($b & 0x7F) << $s;
-            if ($b & 0x80) {
+            if (! ($b & 0x80)) {
                 break;
             }
             $s += 7;
