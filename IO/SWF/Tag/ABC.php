e72a493eab69276fa594642e0cd5e2763bea1369
Yoshihiro Yamazaki <yoya@awm.jp>
2020-02-12 12:18:20 +0900

    swfdump.php -a (abcdump) option.

--- IO/SWF/Tag/ABC.php
+++ IO/SWF/Tag/ABC.php
@@ -10,6 +10,7 @@ if (is_readable('vendor/autoload.php')) {
     require_once 'IO/Bit.php';
 }
 require_once dirname(__FILE__).'/Base.php';
+require_once dirname(__FILE__).'/../ABC.php';
 require_once dirname(__FILE__).'/../Type/String.php';
 
 class IO_SWF_Tag_ABC extends IO_SWF_Tag_Base {
@@ -23,13 +24,21 @@ class IO_SWF_Tag_ABC extends IO_SWF_Tag_Base {
         $this->_Flags = $reader->getUI32LE();
         $this->_Name = IO_SWF_Type_String::parse($reader);
         $this->_ABCData = $reader->getDataUntil(false);
+        $abc = new IO_SWF_ABC();
+        $abc->parse($this->_ABCData);
+        $this->_ABC = $abc;
     }
 
     function dumpContent($tagCode, $opts = array()) {
+        $opts['abcdump'] = ! empty($opts['abcdump']);
         printf("    Flags: 0x%08x", $this->_Flags);
         echo "  Name: ".$this->_Name;
         echo "  ABCData (len:".strlen($this->_ABCData).")";
         echo "\n";
+        if ($opts['abcdump']) {
+            $abc = $this->_ABC;
+            $abc->dump($opts);
+        }
     }
 
     function buildContent($tagCode, $opts = array()) {
--- sample/swfdump.php
+++ sample/swfdump.php
@@ -6,11 +6,11 @@ if (is_readable('vendor/autoload.php')) {
     require 'IO/SWF.php';
 }
 
-$options = getopt("f:hl");
+$options = getopt("f:hla");
 
 if ((isset($options['f']) === false) || (is_readable($options['f']) === false)) {
-    echo "Usage: php swfdump.php -f <swf_file> [-h] [-l]\n";
-    echo "ex) php swfdump.php -f test.swf -h -l\n";
+    echo "Usage: php swfdump.php -f <swf_file> [-h] [-l] [-a]\n";
+    echo "ex) php swfdump.php -f test.swf -h -l -a\n";
     exit(1);
 }
 
@@ -21,12 +21,14 @@ $swf = new IO_SWF();
 $swf->parse($swfdata);
 
 $opts = array();
+
 if (isset($options['h'])) {
     $opts['hexdump'] = true;
 }
 if (isset($options['l'])) {
     $opts['addlabel'] = true;
 }
+$opts['abcdump'] = isset($options['a']);
 
 $swf->dump($opts);
 
