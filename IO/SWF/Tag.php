74fc631d774365182fe5b3ba1e21027150d6d360
Yoshihiro Yamazaki <yoya@awm.jp>
2020-04-13 20:34:21 +0900

    typo: videoFrame => VideoFrame

--- IO/SWF/Tag.php
+++ IO/SWF/Tag.php
@@ -62,7 +62,7 @@ class IO_SWF_Tag {
         59 => array('name' => 'DoInitAction', 'klass' => 'Action', 'version' => 6),
         //
         60 => array('name' => 'DefineVideoStream', 'version' => 6),
-        61 => array('name' => 'videoFrame', 'version' => 6),
+        61 => array('name' => 'VideoFrame', 'version' => 6),
         62 => array('name' => 'DefineFontInfo2', 'version' => 6),
         // 63 missing
         64 => array('name' => 'EnableDebugger2', 'version' => 6),
