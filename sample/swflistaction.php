49638788c376704c2fdc8c235399a47b3a08ffd1
Yoshihiro Yamazaki <yoya@awm.jp>
2020-08-11 00:38:32 +0900

    refine function swflistaction for sprite tag.

--- sample/swflistaction.php
+++ sample/swflistaction.php
@@ -24,9 +24,9 @@ $swf = new IO_SWF_Editor();
 
 $swf->parse($swfdata);
 
-swflistaction($swf->_tags);
+swflistaction($swf->_tags, null);
 
-function swflistaction($swftags) {
+function swflistaction($swftags, $spriteId) {
     $currentFrame = 1;
     foreach ($swftags as $idx => $tag) {
         $tag_code = $tag->code;
@@ -50,7 +50,12 @@ function swflistaction($swftags) {
                 }
             }
             $length = strlen($tag->content);
-            echo "spriteId:0(root)  frame:$currentFrame\t=> instruction:$action_num  length=$length\n";
+            if ($spriteId) {
+                echo "spriteId:$spriteId";
+            } else {
+                echo "spriteId:(root)";
+            }
+            echo "  frame:$currentFrame\t=> instruction:$action_num  length=$length\n";
         }
         if ($tag_code == 39) { // DefineSprite
             if ($tag->parseTagContent() === false) {
@@ -58,32 +63,7 @@ function swflistaction($swftags) {
                 exit(1);
             }
             $spriteId = $tag->tag->_spriteId;
-            $currentFrameInSprite = 1;
-            foreach ($tag->tag->_controlTags as $tag_in_sprite) {
-                $tag_code_in_sprite = $tag_in_sprite->code;
-                if ($tag_code_in_sprite == 1) { // ShowFrame
-                    $currentFrameInSprite++;
-                    continue;
-                }
-                if (($tag_code_in_sprite == 12) || ($tag_code_in_sprite == 59)) { // DoAction, DoInitAction
-                    try {
-                        if ($tag_in_sprite->parseTagContent() === false) {
-                            echo "Unknown Action Tag\n";
-                            exit(1);
-                        }
-                        $action_num = count($tag_in_sprite->tag->_actions);
-                    } catch (IO_Bit_Exception $e) {
-                        if (isset($tag_in_sprite->tag->_actions)) {
-                            $action_num = count($tag_in_sprite->tag->_actions);
-                            $action_num .= "(at least)";
-                        } else {
-                            $action_num = "(parse error)";
-                        }
-                    }
-                    $length = strlen($tag_in_sprite->content);
-                    echo "spriteId:$spriteId  frame:$currentFrameInSprite\t=> instruction:$action_num  length=$length\n";
-                }
-            }
+            swflistaction($tag->tag->_controlTags, $spriteId);
         }
     }
 }
