a49f968577f140462ceac7ec04f3f8c40d150e90
Yoshihiro Yamazaki <yoya@awm.jp>
2020-08-10 20:55:25 +0900

    function swflistaction

--- sample/swflistaction.php
+++ sample/swflistaction.php
@@ -24,66 +24,68 @@ $swf = new IO_SWF_Editor();
 
 $swf->parse($swfdata);
 
-$currentFrame = 1;
+swflistaction($swf->_tags);
 
-foreach ($swf->_tags as $idx => $tag) {
-    $tag_code = $tag->code;
-    if ($tag_code == 1) { // ShowFrame
-        $currentFrame++;
-        continue;
-    }
-    if (($tag_code == 12) || ($tag_code == 59)) { // DoAction, DoInitAction
-        try {
+function swflistaction($swftags) {
+    $currentFrame = 1;
+    foreach ($swftags as $idx => $tag) {
+        $tag_code = $tag->code;
+        if ($tag_code == 1) { // ShowFrame
+            $currentFrame++;
+            continue;
+        }
+        if (($tag_code == 12) || ($tag_code == 59)) { // DoAction, DoInitAction
+            try {
+                if ($tag->parseTagContent() === false) {
+                    echo "Unknown Action Tag\n";
+                    exit(1);
+                }
+                $action_num = count($tag->tag->_actions);
+            } catch (IO_Bit_Exception $e) {
+                if (isset($tag_in_sprite->tag->_actions)) {
+                    $action_num = count($tag_in_sprite->tag->_actions);
+                    $action_num .= "(at least)";
+                } else {
+                    $action_num = "(parse error)";
+                }
+            }
+            $length = strlen($tag->content);
+            echo "spriteId:0(root)  frame:$currentFrame\t=> instruction:$action_num  length=$length\n";
+        }
+        if ($tag_code == 39) { // DefineSprite
             if ($tag->parseTagContent() === false) {
-                echo "Unknown Action Tag\n";
+                echo "Unknown DefineSprite!!!\n";
                 exit(1);
             }
-            $action_num = count($tag->tag->_actions);
-        } catch (IO_Bit_Exception $e) {
-            if (isset($tag_in_sprite->tag->_actions)) {
-                $action_num = count($tag_in_sprite->tag->_actions);
-                $action_num .= "(at least)";
-            } else {
-                $action_num = "(parse error)";
-            }
-        }
-        $length = strlen($tag->content);
-        echo "spriteId:0(root)  frame:$currentFrame\t=> instruction:$action_num  length=$length\n";
-    }
-    if ($tag_code == 39) { // DefineSprite
-        if ($tag->parseTagContent() === false) {
-            echo "Unknown DefineSprite!!!\n";
-            exit(1);
-        }
-        $spriteId = $tag->tag->_spriteId;
-        $currentFrameInSprite = 1;
-        foreach ($tag->tag->_controlTags as $tag_in_sprite) {
-            $tag_code_in_sprite = $tag_in_sprite->code;
-            if ($tag_code_in_sprite == 1) { // ShowFrame
-                $currentFrameInSprite++;
-                continue;
-            }
-            if (($tag_code_in_sprite == 12) || ($tag_code_in_sprite == 59)) { // DoAction, DoInitAction
-                try {
-                    if ($tag_in_sprite->parseTagContent() === false) {
-                        echo "Unknown Action Tag\n";
-                        exit(1);
-                    }
-                    $action_num = count($tag_in_sprite->tag->_actions);
-                } catch (IO_Bit_Exception $e) {
-                    if (isset($tag_in_sprite->tag->_actions)) {
-                        $action_num = count($tag_in_sprite->tag->_actions);
-                        $action_num .= "(at least)";
-                    } else {
-                        $action_num = "(parse error)";
-                    }
+            $spriteId = $tag->tag->_spriteId;
+            $currentFrameInSprite = 1;
+            foreach ($tag->tag->_controlTags as $tag_in_sprite) {
+                $tag_code_in_sprite = $tag_in_sprite->code;
+                if ($tag_code_in_sprite == 1) { // ShowFrame
+                    $currentFrameInSprite++;
+                    continue;
                 }
-                $length = strlen($tag_in_sprite->content);
-                echo "spriteId:$spriteId  frame:$currentFrameInSprite\t=> instruction:$action_num  length=$length\n";
+                if (($tag_code_in_sprite == 12) || ($tag_code_in_sprite == 59)) { // DoAction, DoInitAction
+                    try {
+                        if ($tag_in_sprite->parseTagContent() === false) {
+                            echo "Unknown Action Tag\n";
+                            exit(1);
+                        }
+                        $action_num = count($tag_in_sprite->tag->_actions);
+                    } catch (IO_Bit_Exception $e) {
+                        if (isset($tag_in_sprite->tag->_actions)) {
+                            $action_num = count($tag_in_sprite->tag->_actions);
+                            $action_num .= "(at least)";
+                        } else {
+                            $action_num = "(parse error)";
+                        }
+                    }
+                    $length = strlen($tag_in_sprite->content);
+                    echo "spriteId:$spriteId  frame:$currentFrameInSprite\t=> instruction:$action_num  length=$length\n";
+                }
             }
         }
     }
-
 }
 
 exit(0);
