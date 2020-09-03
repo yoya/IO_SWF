6ae34d1963cd13e48dcaea8a3d352f133587884c
Yoshihiro Yamazaki <yoya@awm.jp>
2020-09-04 01:39:45 +0900

    fix VideoFrame build.

--- IO/SWF/Tag/VideoFrame.php
+++ IO/SWF/Tag/VideoFrame.php
@@ -17,43 +17,54 @@ class IO_SWF_Tag_VideoFrame extends IO_SWF_Tag_Base {
     	$reader->input($content);
         $this->_StreamID = $reader->getUI16LE();
         $this->_FrameNum = $reader->getUI16LE();
+        $this->_VideoData = $reader->getDataUntil(false);
         if (isset($opts['_CodecID'])) {
             $this->_CodecID = $codecID = $opts['_CodecID'];
+            $readerVideo = new IO_Bit();
+            $readerVideo->input($this->_VideoData);
             switch ($codecID) {
-            case 5:  // VP6SWFALPHAVIDEOPACKET
-                $this->_OffsetToAlpha = $reader->getUIBits(24);
-                $this->_Data = $reader->getData($this->_OffsetToAlpha);
-                $this->_Alphadata = $reader->getDataUntil(false);
+            case 4:  // VP6SWFVIDEOPACKET
+                $this->_Data = $readerVideo->getDataUntil(false);
                 break;
+            case 5:  // VP6SWFALPHAVIDEOPACKET
+                $this->_OffsetToAlpha = $readerVideo->getUIBits(24);
+                $this->_Data = $readerVideo->getData($this->_OffsetToAlpha);
+                $this->_Alphadata = $readerVideo->getDataUntil(false);
+                break;
+            case 2:  // H263VIDEOPACKET
+            case 3:  // SCREENVIDEOPACKET
+            case 6:  // SCREENV2VIDEOPACKET
             default:
-                $this->_VideoData = $reader->getDataUntil(false);
                 break;
             }
-        } else {
-            $this->_VideoData = $reader->getDataUntil(false);
         }
     }
 
     function dumpContent($tagCode, $opts = array()) {
-        $data_len = strlen($this->_VideoData);
+        $vdata_len = strlen($this->_VideoData);
         echo "    StreamID: {$this->_StreamID}";
         echo "  FrameNum: {$this->_FrameNum}";
+        echo "  VideoData: (len=$vdata_len)\n";
         if ($this->_CodecID) {
             $codecID = $this->_CodecID;
             switch ($codecID) {
+            case 4:  // VP6SWFVIDEOPACKET
+                $data_len = strlen($this->_Data);
+                echo "    Data: (len=$data_len)\n";
+                break;
             case 5:  // VP6SWFALPHAVIDEOPACKET
-                echo "  OffsetToAlpha: {$this->_OffsetToAlpha}\n";
                 $data_len = strlen($this->_Data);
                 $alphadata_len = strlen($this->_Alphadata);
-                echo "    Data: (len=$data_len)";
+                echo "    OffsetToAlpha: {$this->_OffsetToAlpha}";
+                echo "  Data: (len=$data_len)";
                 echo "  AlphaData: (len=$alphadata_len)\n";
                 break;
+            case 2:  // H263VIDEOPACKET
+            case 3:  // SCREENVIDEOPACKET
+            case 6:  // SCREENV2VIDEOPACKET
             default:
-                echo "  VideoData: (len=$data_len)\n";
                 break;
             }
-        } else {
-            echo "  VideoData: (len=$data_len)\n";
         }
     }
 
