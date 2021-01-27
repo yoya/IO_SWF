d1d53472951dc449d06f6c42ca2980fecdb7daf0
Yoshihiro Yamazaki <yoya@awm.jp>
2021-01-28 01:11:27 +0900

    dump video stream with codec name.

--- IO/SWF/Tag/VideoStream.php
+++ IO/SWF/Tag/VideoStream.php
@@ -13,6 +13,19 @@ class IO_SWF_Tag_VideoStream extends IO_SWF_Tag_Base {
     var $_Width, $_Height;
     var $_VideoFlags;
     var $_CodecID;
+
+    var $_codecIDs = [
+        2 => "H.263", 3 => "Screen Video",
+        4 => "On2 VP6 SWF", 5=> "On2 VP6 SWF with Alpha",
+        6 =>"Screen Video v2"
+    ];
+
+    function getCodecName($id) {
+        if (isset($this->_codecIDs[$id])) {
+                return $this->_codecIDs[$id];
+        }
+        return "Unknown";
+    }
     
     function parseContent($tagCode, $content, $opts = array()) {
         $reader = new IO_Bit();
@@ -29,8 +42,10 @@ class IO_SWF_Tag_VideoStream extends IO_SWF_Tag_Base {
         echo "    CharacterID: {$this->_CharacterID}";
         echo "  NumFrames: {$this->_NumFrames}";
         echo "  Width: {$this->_Width}  Height: {$this->_Height}\n";
-        echo "    VideoFlags: {$this->_VideoFlags}  ";
-        echo "  CodecID: {$this->_CodecID}\n";
+        echo "    VideoFlags: {$this->_VideoFlags}";
+        $codecID = $this->_CodecID;
+        $codecName = $this->getCodecName($codecID);
+        echo "  CodecID: $codecID ($codecName)\n";
     }
 
     function buildContent($tagCode, $opts = array()) {
