9ce6d37bcab5d662b2db4faa64796ead37b6d7f6
Yoshihiro Yamazaki <yoya@awm.jp>
2021-02-09 04:01:33 +0900

    error handling. www/swftov4.php

--- www/swftov4.php
+++ www/swftov4.php
@@ -7,7 +7,8 @@ if (is_readable('vendor/autoload.php')) {
 }
 
 $tmp_name = $_FILES["swffile"]["tmp_name"];
-$filename = $_FILES["swffile"]["name"];
+$filepath = pathinfo($_FILES["swffile"]["name"]);
+$filename = $filepath['filename'] . "-tov4.swf";
 
 $swfVersion = 4;
 $limitSwfVersion = $swfVersion;
@@ -16,11 +17,16 @@ $opts = [ 'preserveStyleState' => true, 'eliminate' => true ];
 $swfdata = file_get_contents($tmp_name);
 
 $swf = new IO_SWF_Editor();
-$swf->parse($swfdata, $opts);
-$swf->downgrade($swfVersion, $limitSwfVersion, $opts);
 
-$filepath = pathinfo($filename);
-$filename = $filepath['filename'] . "-tov4.swf";
-header("Content-Type: application/x-shockwave-flash");
-header("Content-Disposition: attachment; filename=$filename");
-echo $swf->build($opts);
+try {
+    $swf->parse($swfdata, $opts);
+    $swf->downgrade($swfVersion, $limitSwfVersion, $opts);
+    $output = $swf->build($opts);
+    header("Content-Type: application/x-shockwave-flash");
+    header("Content-Disposition: attachment; filename=$filename");
+    echo $output;
+} catch (Exception $e) {
+    header("Content-Type: plain/text");
+    header("Content-Disposition: attachment; filename=error.txt");
+    echo $e->getMessage();
+}
