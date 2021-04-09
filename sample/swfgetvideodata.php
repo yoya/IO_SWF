5034442a6219b4b55e55471ba4a24b73f9900325
Yoshihiro Yamazaki <yoya@awm.jp>
2021-04-10 03:37:39 +0900

    add swfgetvideo.php options. -c color only -a alpha only. obsolute -A (no alpha)

--- sample/swfgetvideodata.php
+++ sample/swfgetvideodata.php
@@ -15,14 +15,23 @@ function usage() {
     echoerr("Usage: php swfgetvideodata.php -f <swf_file> -i <video_id> [-o offset] [-n numFrames] [-A]\n");
     echoerr("ex) php swfgetvideodata.php -f video.swf -i 1 > data.vp6\n");
     echoerr("ex) php swfgetvideodata.php -f video.swf -i 1 -n 3 > data-3frames.vp6\n");
-    echoerr("ex) php swfgetvideodata.php -f video.swf -i 1 -A > data-noalpha.vp6\n");
+    echoerr("ex) php swfgetvideodata.php -f video.swf -i 1 -c > data-color.vp6\n");
+    echoerr("ex) php swfgetvideodata.php -f video.swf -i 1 -a > data-alpha.vp6\n");
 }
-$options = getopt("f:i:o:n:A");
+
+$options = getopt("f:i:o:n:ca");
 
 if (isset($options['f']) && is_readable($options['f']) &&
     isset($options['i']) && is_numeric($options['i'])) {
     // OK
 } else {
+    echo "must be set -f <file> and -i <number>\n";
+    usage();
+    exit(1);
+}
+
+if (isset($options['c']) && is_readable($options['a'])) {
+    echo "exclusive option -c and -a \n";
     usage();
     exit(1);
 }
@@ -38,9 +47,10 @@ assert(isset($argv[2]));
 $video_id = intval($options['i']);
 $offsetFrame = isset($options['o'])? intval($options['o']): 0;
 $numFrames = isset($options['n'])? intval($options['n']): null;
-$no_alpha = isset($options['A']);
+$color_only = isset($options['c']);
+$alpha_only = isset($options['a']);
 
-echoerr("filename:$filename video_id:$video_id offsetFrame:$offsetFrame numFrames:$numFrames no_alpha:$no_alpha\n");
+echoerr("filename:$filename video_id:$video_id offsetFrame:$offsetFrame numFrames:$numFrames color_only:".($color_only?"true":"false")." alpha_only:".($alpha_only?"true":"false")."\n");
 
 $swf = new IO_SWF_Editor();
 $swf->parse($swfdata);
@@ -79,27 +89,34 @@ if ($videoStream === false) {
 }
 if ($videoframes === false) {
     echoerr("getVideoFrames($video_id) failed\n");
-    exit(1);
+   exit(1);
 }
-if ($no_alpha || (count($videoframes) === 0)) {
+if ($color_only || (count($videoframes) === 0)) {
     $has_alpha = false;
 } else {
     $has_alpha = isset($videoframes[0]["AlphaData"]);
 }
 
-$ae = new IO_SWF_AE($swf->_headers, $videoStream, $has_alpha);
+$ae = new IO_SWF_AE($swf->_headers, $videoStream, $has_alpha && (!$alpha_only));
 
 foreach ($videoframes as $idx => $frame) {
-    if ($has_alpha) {
+    if ($alpha_only) {
         if (! isset($frame["AlphaData"])) {
             throw new Exception("internal error: no AlphaData in VideoFrame");
         }
         $ae->addFrame($frame["AlphaData"], true);
+    } else {
+        if ($has_alpha) {
+            if (! isset($frame["AlphaData"])) {
+                throw new Exception("internal error: no AlphaData in VideoFrame");
+            }
+            $ae->addFrame($frame["AlphaData"], true);
+        }
+        if (! isset($frame["Data"])) {
+            throw new Exception("internal error: no Data in VideoFrame");
+        }
+        $ae->addFrame($frame["Data"], false);
     }
-    if (! isset($frame["Data"])) {
-        throw new Exception("internal error: no Data in VideoFrame");
-    }
-    $ae->addFrame($frame["Data"], false);
 }
 
 echo $ae->output();
