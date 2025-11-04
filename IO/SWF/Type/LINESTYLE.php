e76699833ca1b69f1d4b14afb5417e2b521fb764
Yoshihiro Yamazaki <yoya@awm.jp>
2025-11-04 23:25:24 +0900

    LINESTYLE の debug メッセージを stdout でなく stderr に出力する

--- IO/SWF/Type/LINESTYLE.php
+++ IO/SWF/Type/LINESTYLE.php
@@ -59,8 +59,8 @@ class IO_SWF_Type_LINESTYLE implements IO_SWF_Type {
         $isMorph = ($tagCode == 46) || ($tagCode == 84);
         if ($opts['debug']) {
             $tagName = IO_SWF_Tag::getTagInfo($tagCode, "name");
-            printf("Type_LINESTYLE::build: shapeId:%d tagCode:%d(%s)\n",
-                   $opts['shapeId'], $tagCode, $tagName);
+            fprintf(STDERR, "Type_LINESTYLE::build: shapeId:%d tagCode:%d(%s)\n",
+                    $opts['shapeId'], $tagCode, $tagName);
         }
         if ($isMorph === false) {
             $writer->putUI16LE($lineStyle['Width']);
